c491652c49132f7f83c8d9c90cf5ab1cbded68ea
victornpb <3372598+victornpb@users.noreply.github.com>
2023-10-31 09:00:07 -0300

    enabling 4 slots for saving position

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2278,7 +2278,7 @@
 //
 // G60/G61 Position Save and Return
 //
-//#define SAVED_POSITIONS 1         // Each saved position slot costs 12 bytes
+#define SAVED_POSITIONS 4         // Each saved position slot costs 12 bytes //@TINYMOD
 
 //
 // G2/G3 Arc Support
